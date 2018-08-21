2880e1f1454415ab8f9f8e0add7b999e8b1cc1d2
Kronos <1963024305@qq.com>
2018-08-21 18:03:40 +0800

    基本模块已完成(成就模块未作) 待测试

---
+++ ToDoList/src/com/ToDoList/control/deletesubtask.java
@@ -0,0 +1,52 @@
+package com.ToDoList.control;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.ToDoList.model.SubTask;
+
+public class deletesubtask extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	private String user_id = null;
+	private String subtask_id = null;
+	private SubTask subtask = null;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		this.doPost(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		request.setCharacterEncoding("GBK"); //编码统一
+		user_id = request.getParameter("user_id");
+		subtask_id = request.getParameter("subtask_id");
+		/*设置回复格式*/
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE"); 
+		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, client_id, uuid, Authorization"); 
+		response.setContentType("application/json; charList=utf-8");
+		PrintWriter out = response.getWriter();
+		subtask = new SubTask();
+		try {
+			boolean result = subtask.delete_subtask(user_id,subtask_id);
+			if(result) {
+				/*删除成功*/
+				String jsonStr =  "{\"successfully\":\"001\"}";
+				out.write(jsonStr);
+				out.close();
+			}else {
+				/*删除失败*/
+				String jsonStr =  "{\"error\":\"0x777\"}";
+				out.write(jsonStr);
+				out.close();
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
